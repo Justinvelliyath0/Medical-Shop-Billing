5318a20a79bdc12eaf84af3c243ec83753382487
justin <justinvelliyath22@gmail.com>
2025-04-28 17:26:49 +0530

    feat: update password added in user update.

--- users/serializers.py
+++ users/serializers.py
@@ -1,3 +1,6 @@
+import re
+
+from django.contrib.auth.hashers import make_password
 from rest_framework import serializers
 
 from users.models import User
@@ -21,7 +24,38 @@ class UserUpdateSerializer(serializers.ModelSerializer):
     name = serializers.CharField(required=False)
     role = serializers.CharField(required=False)
     username = serializers.CharField(required=False)
+    password = serializers.CharField(
+        write_only=True, required=False, min_length=6
+    )
+    confirm_password = serializers.CharField(
+        write_only=True, required=False, min_length=6
+    )
 
     class Meta:
         model = User
-        fields = ("name", "role", "username")
+        fields = ("name", "role", "username", "password", "confirm_password")
+
+    def validate_password(self, value):
+        if value:
+            pattern = re.compile(r"^(?=.*[a-zA-Z])(?=.*\d).{6,}$")
+            if not pattern.match(value):
+                raise serializers.ValidationError(
+                    "Password must contain at least one letter, "
+                    "one digit, and be at least 6 characters long."
+                )
+        return value
+
+    def validate(self, data):
+        # Ensure password and confirm_password match
+        password = data.get("password")
+        confirm_password = data.get("confirm_password")
+        if password and not confirm_password:
+            raise serializers.ValidationError("Please enter confirm_password.")
+
+        if password and confirm_password and password != confirm_password:
+            raise serializers.ValidationError(
+                "Password and confirm password do not match."
+            )
+        if password:
+            data["password"] = make_password(password)
+        return data
